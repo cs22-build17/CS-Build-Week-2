9d6288c3c666da1cbccc044ca06203496363f7cb
Pablo Ogata <pabloogata@gmail.com>
2019-11-18 19:25:58 -0500

    prject set-up

---
+++ traversing.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 18 19:23:27 2019
+
+@author: pablo
+"""
+
+
+# api-endpoint
+init = "https://lambda-treasure-hunt.herokuapp.com/api/adv/init/"
+move = "https://lambda-treasure-hunt.herokuapp.com/api/adv/move/"
+take = "https://lambda-treasure-hunt.herokuapp.com/api/adv/take/"
