dc9b7043bcb56f93637c5d5cdd9cb7ff7770e74d
Pablo Ogata <pabloogata@gmail.com>
2019-11-19 14:14:25 -0500

    todo

--- traversing.py
+++ traversing.py
@@ -5,6 +5,10 @@ Created on Mon Nov 18 19:23:27 2019
 @author: pablo
 """
 
+import request
+import time
+
+
 class Stack():
     def __init__(self):
         self.stack = []
@@ -27,3 +31,16 @@ take = "https://lambda-treasure-hunt.herokuapp.com/api/adv/take/"
 changeName = "https://lambda-treasure-hunt.herokuapp.com/api/adv/change_name/"
 sale = "https://lambda-treasure-hunt.herokuapp.com/api/adv/sell/"
 examineItem = "https://lambda-treasure-hunt.herokuapp.com/api/adv/examine/"
+
+
+#json object that contains room information
+
+#inplement graph traversal and building
+
+#account visited room path
+
+#reverse direction incase needs to go back.
+
+#account cooldown time.
+
+#request to the server
